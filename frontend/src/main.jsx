6a8a15501415816f76f41ad67f6048028c533c94
ezio4518 <mdgufran4518@gmail.com>
2025-03-31 03:53:03 +0530

    add initial page components and assets; update index.html title and configure Tailwind CSS

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import ShopContextProvider from './context/ShopContext.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  //removed <React.StrictMode> to use third party libraries and avoid double rendering
+  <BrowserRouter>
+    <ShopContextProvider> {/* to get context api support in whole project */}
+      <App />
+    </ShopContextProvider>
+  </BrowserRouter>,
 )
