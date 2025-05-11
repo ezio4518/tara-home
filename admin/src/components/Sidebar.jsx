a87f41550b25bb673fa368fa811b95829e5e3a34
ezio4518 <mdgufran4518@gmail.com>
2025-05-11 18:02:58 +0530

    Refactor components for improved styling and functionality
    - Updated OurPolicy component to enhance text color and styling.
    - Refined Login page with better input handling and styling adjustments.
    - Enhanced Orders page layout and improved text color for better visibility.
    - Improved PlaceOrder page with better form handling and payment method selection.
    - Updated Product page for better layout and styling consistency.

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -7,19 +7,19 @@ const Sidebar = () => {
     <div className='w-[18%] min-h-screen border-r-2'>
         <div className='flex flex-col gap-4 pt-6 pl-[20%] text-[15px]'>
 
-            <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to="/add">
+            <NavLink className='flex items-center gap-3 border border-[#A1876F] border-r-0 px-3 py-2 rounded-l' to="/add">
                 <img className='w-5 h-5' src={assets.add_icon} alt="" />
-                <p className='hidden md:block'>Add Items</p>
+                <p className='hidden md:block text-[#40350A]'>Add Items</p>
             </NavLink>
 
-            <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to="/list">
+            <NavLink className='flex items-center gap-3 border border-[#A1876F] border-r-0 px-3 py-2 rounded-l' to="/list">
                 <img className='w-5 h-5' src={assets.order_icon} alt="" />
-                <p className='hidden md:block'>List Items</p>
+                <p className='hidden md:block text-[#40350A]'>List Items</p>
             </NavLink>
 
-            <NavLink className='flex items-center gap-3 border border-gray-300 border-r-0 px-3 py-2 rounded-l' to="/orders">
+            <NavLink className='flex items-center gap-3 border border-[#A1876F] border-r-0 px-3 py-2 rounded-l' to="/orders">
                 <img className='w-5 h-5' src={assets.order_icon} alt="" />
-                <p className='hidden md:block'>Orders</p>
+                <p className='hidden md:block text-[#40350A]'>Orders</p>
             </NavLink>
 
         </div>
